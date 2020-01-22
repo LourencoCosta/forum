49a33b41387104e7a7c2a87af258a58fdad3be88
rlc <rafaelconteudossi@gmail.com>
2020-01-21 22:08:02 -0300

    Implementacao e configuracao do sprin security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$GULpJNkbp2pTI8W2fKhlkOlS6.hirGVfZZ3QPJp/Gs22m7uu/W56C');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
